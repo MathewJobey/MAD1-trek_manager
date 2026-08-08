7ac5649361906bb724da35cf78f42602d2d435bf
Mathew Jobey <mathewjobey17@gmail.com>
2026-08-08 23:51:01 +0530

    Add admin dashboard route

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -8,7 +8,7 @@ from functools import wraps
 
 admin_bp = Blueprint("admin", __name__, url_prefix='/admin')
 
-#CUSTOM DECORATOR- to restrict users through admin routes
+#CUSTOM DECORATOR- to restrict users through admin routes like how login_required deco already exists
 def admin_required(f):
     @wraps(f)
     def decorator_function(*args,**kwargs):#takes in arguments and checks if user is 1. logged in OR 2. role=admin
@@ -17,4 +17,18 @@ def admin_required(f):
             return redirect(url_for('auth.login'))
         return f(*args,**kwargs)
     return decorator_function
+
+#ADMIN DASHBOARD
+@admin_bp.route('/dashboard')
+@login_required
+@admin_required
+def dashboard():
+
+    #getting values from DB to send to FRONTEND {1.pending approval, 2. all users, 3. all treks, 4. all bookings}
+    pending_staff=User.query.filter_by(role='staff',is_approved=False).all()
+    all_users=User.query.filter(User.role!='admin').all() #all except the admin
+    all_treks=Trek.query.all()
+    all_bookings=Booking.query.all()
+
+    return render_template('admin/dashboard.html', pending_staff=pending_staff,users=all_users,treks=all_treks,bookings=all_bookings)
 
