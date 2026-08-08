7c8f6f7360900e24e2e229c15c2b89871d055c95
Mathew Jobey <mathewjobey17@gmail.com>
2026-08-08 23:32:16 +0530

    admin required deco function

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,9 +1,20 @@
-from flask import Blueprint
+from app.models import User, Trek, Booking
+from app import db
+from flask import Blueprint,render_template,redirect,url_for,flash,request
+from flask_login import login_required, current_user
+from functools import wraps
 
 
-admin_bp = Blueprint("admin", __name__)
 
+admin_bp = Blueprint("admin", __name__, url_prefix='/admin')
 
-@admin_bp.route("/")
-def dashboard():
-    return "Admin dashboard"
+#CUSTOM DECORATOR- to restrict users through admin routes
+def admin_required(f):
+    @wraps(f)
+    def decorator_function(*args,**kwargs):#takes in arguments and checks if user is 1. logged in OR 2. role=admin
+        if not current_user.is_authenticated or current_user.role !='admin':
+            flash('Access Denied! Admin Privileges Required.','danger')
+            return redirect(url_for('auth.login'))
+        return f(*args,**kwargs)
+    return decorator_function
+
